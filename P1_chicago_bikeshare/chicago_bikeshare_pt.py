a70e8c93536d429151ca8df169f98b4efdb2ae14
Leonardo Naysinger Machado <leonaysinger@gmail.com>
2018-08-26 11:23:14 -0300

    Projetos finais

---
+++ P1_chicago_bikeshare/chicago_bikeshare_pt.py
@@ -0,0 +1,329 @@
+# coding: utf-8
+
+# Começando com os imports
+import csv
+import matplotlib.pyplot as plt
+
+# Vamos ler os dados como uma lista
+print("Lendo o documento...")
+with open("chicago.csv", "r") as file_read:
+    reader = csv.reader(file_read)
+    data_list = list(reader)
+print("Ok!")
+
+# Vamos verificar quantas linhas nós temos
+print("Número de linhas:")
+print(len(data_list))
+
+# Imprimindo a primeira linha de data_list para verificar se funcionou.
+print("Linha 0: ")
+print(data_list[0])
+# É o cabeçalho dos dados, para que possamos identificar as colunas.
+
+# Imprimindo a segunda linha de data_list, ela deveria conter alguns dados
+print("Linha 1: ")
+print(data_list[1])
+
+input("Aperte Enter para continuar...")
+# TAREFA 1
+# TODO: Imprima as primeiras 20 linhas usando um loop para identificar os dados.
+print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
+for line in data_list[:20] :
+    print(line)
+
+# Vamos mudar o data_list para remover o cabeçalho dele.
+data_list = data_list[1:]
+
+# Nós podemos acessar as features pelo índice
+# Por exemplo: sample[6] para imprimir gênero, ou sample[-2]
+
+input("Aperte Enter para continuar...")
+# TAREFA 2
+# TODO: Imprima o `gênero` das primeiras 20 linhas
+
+print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
+for line in data_list[:20] :
+    print(line[6])
+
+# Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
+# Mas ainda é difícil pegar uma coluna em uma lista. Exemplo: Lista com todos os gêneros
+
+input("Aperte Enter para continuar...")
+# TAREFA 3
+# TODO: Crie uma função para adicionar as colunas(features) de uma lista em outra lista, na mesma ordem
+# def column_to_list(param1: list, param2: int) -> list:
+def column_to_list(data, index):
+    """
+              Função para adicionar as colunas de uma lista em outra.
+              Argumentos:
+                  param1: data. dataset
+                  param2: index. feature da lista que deverá ser adicionada em outra lista.
+              Retorna:
+                  Uma nova lista contendo somente a feature selecionada de outra.
+
+              """
+    column_list = []
+    for line in data:
+       column_list.append(line[index])
+    # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
+    return column_list
+
+
+# Vamos checar com os gêneros se isso está funcionando (apenas para os primeiros 20)
+print("\nTAREFA 3: Imprimindo a lista de gêneros das primeiras 20 amostras")
+print(column_to_list(data_list, -2)[:20])
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert type(column_to_list(data_list, -2)) is list, "TAREFA 3: Tipo incorreto retornado. Deveria ser uma lista."
+assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorreto retornado."
+assert column_to_list(data_list, -2)[0] == "" and column_to_list(data_list, -2)[1] == "Male", "TAREFA 3: A lista não coincide."
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# Agora sabemos como acessar as features, vamos contar quantos Male (Masculinos) e Female (Femininos) o dataset tem
+# TAREFA 4
+# TODO: Conte cada gênero. Você não deveria usar uma função parTODO isso.
+male = 0
+female = 0
+
+for line in data_list:
+    if (line[6] == 'Male') :
+        male = male + 1
+    elif (line[6] == 'Female') :
+        female = female + 1
+
+# Verificando o resultado
+print("\nTAREFA 4: Imprimindo quantos masculinos e femininos nós encontramos")
+print("Masculinos: ", male, "\nFemininos: ", female)
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert male == 935854 and female == 298784, "TAREFA 4: A conta não bate."
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# Por que nós não criamos uma função parTODO isso?
+# TAREFA 5
+# TODO: Crie uma função para contar os gêneros. Retorne uma lista.
+# Isso deveria retornar uma lista com [count_male, count_female] (exemplo: [10, 15] significa 10 Masculinos, 15 Femininos)
+# def count_gender(param1: list) -> list:
+def count_gender(data_list):
+    """
+        Função para para contar os gêneros.
+        Argumentos:
+            param1: data_list. Todo o dataset
+        Retorna:
+            Uma nova lista contendo o somátorio de gênero
+
+        """
+    male = 0
+    female = 0
+
+    for line in data_list:
+        if (line[6] == 'Male'):
+            male = male + 1
+        elif (line[6] == 'Female'):
+            female = female + 1
+
+    return [male, female]
+
+print("\nTAREFA 5: Imprimindo o resultado de count_gender")
+print(count_gender(data_list))
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert type(count_gender(data_list)) is list, "TAREFA 5: Tipo incorreto retornado. Deveria retornar uma lista."
+assert len(count_gender(data_list)) == 2, "TAREFA 5: Tamanho incorreto retornado."
+assert count_gender(data_list)[0] == 935854 and count_gender(data_list)[1] == 298784, "TAREFA 5: Resultado incorreto no retorno!"
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# Agora que nós podemos contar os usuários, qual gênero é mais prevalente?
+# TAREFA 6
+# TODO: Crie uma função que pegue o gênero mais popular, e retorne este gênero como uma string.
+# Esperamos ver "Masculino", "Feminino", ou "Igual" como resposta.
+# def most_popular_gender(param1: list) -> str:
+def most_popular_gender(data_list):
+    """
+        Função que pega o gênero mais popular.
+        Argumentos:
+            param1: data_list. Todo o dataset
+        Retorna:
+             Uma nova string indicando qual o gênero mais popular
+
+        """
+    answer = ""
+    gender_count = count_gender(data_list)
+    if (gender_count[0] > gender_count[1]) :
+        answer = 'Masculino'
+    else:
+        answer = 'Feminino'
+    return answer
+
+
+print("\nTAREFA 6: Qual é o gênero mais popular na lista?")
+print("O gênero mais popular na lista é: ", most_popular_gender(data_list))
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert type(most_popular_gender(data_list)) is str, "TAREFA 6: Tipo incorreto no retorno. Deveria retornar uma string."
+assert most_popular_gender(data_list) == "Masculino", "TAREFA 6: Resultado de retorno incorreto!"
+# -----------------------------------------------------
+
+# Se tudo está rodando como esperado, verifique este gráfico!
+gender_list = column_to_list(data_list, -2)
+types = ["Male", "Female"]
+quantity = count_gender(data_list)
+y_pos = list(range(len(types)))
+plt.bar(y_pos, quantity)
+plt.ylabel('Quantidade')
+plt.xlabel('Gênero')
+plt.xticks(y_pos, types)
+plt.title('Quantidade por Gênero')
+plt.show(block=True)
+
+input("Aperte Enter para continuar...")
+# TAREFA 7
+# TODO: Crie um gráfico similar para user_types. Tenha certeza que a legenda está correta.
+print("\nTAREFA 7: Verifique o gráfico!")
+user_types = column_to_list(data_list, -3)
+types = list(set(user_types))
+quantity = [user_types.count(types[0]), user_types.count(types[1]), user_types.count(types[2])]
+y_pos = list(range(len(types)))
+plt.bar(y_pos, quantity)
+plt.ylabel('Quantidade')
+plt.xlabel('Tipo Usuário')
+plt.xticks(y_pos, types)
+plt.title('Quantidade por Tipo Usuário')
+plt.show(block=True)
+
+input("Aperte Enter para continuar...")
+# TAREFA 8
+# TODO: Responda a seguinte questão
+male, female = count_gender(data_list)
+print("\nTAREFA 8: Por que a condição a seguir é Falsa?")
+print("male + female == len(data_list):", male + female == len(data_list))
+answer = "Porque há dados faltando a respeito de gênero. Há linhas que não possuem gênero."
+print("resposta:", answer)
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert answer != "Escreva sua resposta aqui.", "TAREFA 8: Escreva sua própria resposta!"
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# Vamos trabalhar com trip_duration (duração da viagem) agora. Não conseguimos tirar alguns valores dele.
+# TAREFA 9
+# TODO: Ache a duração de viagem Mínima, Máxima, Média, e Mediana.
+# Você não deve usar funções prontas parTODO isso, como max() e min().
+# def calculate_median(param1: list) -> int:
+def calculate_median(dataset):
+    """
+            Função que calcula a mediana de uma lista.
+            Argumentos:
+                param1: dataset. Todo o dataset
+            Retorna:
+                 Um inteiro indicando qual a mediana
+
+            """
+    median = 0
+    sorted_dataset = sorted(dataset)
+    size_of_dataset = len(trip_duration_list)
+
+    if (len(dataset) % 2 == 0):
+        median = sum(sorted_dataset[size_of_dataset // 2 - 1:size_of_dataset // 2 + 1]) / 2.0
+    else:
+        median = sorted_dataset[size_of_dataset // 2]
+    return median
+
+trip_duration_list = column_to_list(data_list, 2)
+min_trip = 0.
+max_trip = 0.
+mean_trip = 0.
+median_trip = 0.
+sum_all = 0.
+trip_duration_list = [float(trip_duration) for trip_duration in trip_duration_list]
+
+min_trip = trip_duration_list[0]
+max_trip = trip_duration_list[0]
+
+for trip_duration in trip_duration_list:
+    if trip_duration < min_trip:
+        min_trip = trip_duration
+
+    if trip_duration > max_trip:
+        max_trip = trip_duration
+
+    sum_all = sum_all + trip_duration
+
+mean_trip = sum_all / len(trip_duration_list)
+median_trip = calculate_median(trip_duration_list)
+
+print("\nTAREFA 9: Imprimindo o mínimo, máximo, média, e mediana")
+print("Min: ", min_trip, "Max: ", max_trip, "Média: ", mean_trip, "Mediana: ", median_trip)
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert round(min_trip) == 60, "TAREFA 9: min_trip com resultado errado!"
+assert round(max_trip) == 86338, "TAREFA 9: max_trip com resultado errado!"
+assert round(mean_trip) == 940, "TAREFA 9: mean_trip com resultado errado!"
+assert round(median_trip) == 670, "TAREFA 9: median_trip com resultado errado!"
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# TAREFA 10
+# Gênero é fácil porque nós temos apenas algumas opções. E quanto a start_stations? Quantas opções ele tem?
+# TODO: Verifique quantos tipos de start_stations nós temos, usando set()
+user_types = list(set(column_to_list(data_list, 3)))
+
+print("\nTAREFA 10: Imprimindo as start stations:")
+print(len(user_types))
+print(user_types)
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert len(user_types) == 582, "TAREFA 10: Comprimento errado de start stations."
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# TAREFA 11
+# Volte e tenha certeza que você documenteou suas funções. Explique os parâmetros de entrada, a saída, e o que a função faz. Exemplo:
+# def new_function(param1: int, param2: str) -> list:
+"""
+      Função de exemplo com anotações.
+      Argumentos:
+          param1: O primeiro parâmetro.
+          param2: O segundo parâmetro.
+      Retorna:
+          Uma lista de valores x.
+
+      """
+
+input("Aperte Enter para continuar...")
+# TAREFA 12 - Desafio! (Opcional)
+# TODO: Crie uma função para contar tipos de usuários, sem definir os tipos
+# para que nós possamos usar essa função com outra categoria de dados.
+print("Você vai encarar o desafio? (yes ou no)")
+answer = "yes"
+
+# def count_items(param1: list) -> tuple:
+def count_items(column_list):
+    """
+        Função que calcula os tipos de usuário de acordo com seus tipos.
+        Argumentos:
+            param1: column_list. Coluna inteira do dataset
+        Retorna:
+            tupla contendo os tipos de dados e a contagems destes
+
+        """
+    item_types = list(set(column_list))
+    count_items = []
+
+    for index, item in enumerate(item_types):
+        count_items.append(column_list.count(item_types[index]))
+    return item_types, count_items
+
+
+if answer == "yes":
+    # ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+    column_list = column_to_list(data_list, -2)
+    types, counts = count_items(column_list)
+    print("\nTAREFA 11: Imprimindo resultados para count_items()")
+    print("Tipos:", types, "Counts:", counts)
+    assert len(types) == 3, "TAREFA 11: Há 3 tipos de gênero!"
+    assert sum(counts) == 1551505, "TAREFA 11: Resultado de retorno incorreto!"
+    # -----------------------------------------------------
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# fundamentos_ai_machine_learning
+Nanodegree Fundamentos em AI & Machine Learning
+
+## Projeto P1: Explore os dados de bicicletas compartilhadas de Chicago
+Neste projeto, você utilizará o Python para explorar dados relacionados aos sistemas de compartilhamento de bicicletas nas três maiores cidades dos Estados Unidos: Chicago, Nova Iorque e Washington. Você escreverá um código para importar dados e responder a perguntas interessantes sobre eles, por meio da computação de estatísticas descritivas. Você também vai escrever um script que recebe uma entrada bruta para criar uma experiência interativa no terminal, a fim de apresentar estas estatísticas.
+
+#######################################################################################################
+
+
+## Projeto P2: Prevendo o preço de imóveis de Boston
+
+## Visão geral do projeto
+Neste projeto, você aplicará conceitos básicos de Machine Learning em dados coletados de preços de casas em Boston, em Massachusetts, para prever o preço de venda de uma casa nova. Primeiro, você irá explorar os dados para obter atributos importantes  e estatísticas descritivas sobre o conjunto de dados. Depois, dividirá adequadamente os dados entre dois subconjuntos, o de testes e o de treinamento, e determinará uma métrica adequada para esse problema. Você, então, analisará o desempenho de um algoritmo de aprendizagem com parâmetros variados e tamanho do conjunto de treinamento. Isso permitirá que você escolha o modelo ótimo que melhor generaliza os dados desconhecidos. Por último, você testará o modelo ótimo em uma nova amostra e comparará os preços de venda previstos com as suas estatísticas.
+
+## Destaques do projeto
+Este projeto foi projetado para que você se familiarize em trabalhar com conjuntos de dados em Python e aplicar técnicas básicas de Machine Learning utilizando NumPy e Scikit-Learn. Antes de saber usar muitos dos algoritmos disponíveis na biblioteca sklearn, será de grande ajuda, primeiro, praticar análise e interpretação do desempenho de seu modelo.
+
+O que você aprenderá ao concluir este projeto:
+
+- Como utilizar o NumPy para investigar os features latentes de um conjunto de dados.
+- Como analisar vários gráficos de desempenho de aprendizagem para variância e viés.
+- Como determinar o modelo que faz as melhores estimativas para dados desconhecidos.
+- Como avaliar o desempenho do modelo em dados desconhecidos utilizando dados anteriores.
+
+## Descrição
+O mercado imobiliário de Boston é altamente competitivo e você quer ser o melhor corretor de imóveis da região. Para competir com seus colegas, você decidiu elencar alguns conceitos básicos de Machine Learning para ajudar você e seu cliente a acharem o melhor preço de venda para a casa dele. Com sorte, você se deparou com o conjunto de dados de habitação de Boston, que contém dados agregados de várias características para casas das comunidades da Grande Boston, incluindo o valor médio das casas para cada uma das regiões. Sua tarefa é construir um modelo ótimo baseado na análise das estatísticas com as ferramentas disponíveis. Esse modelo será, então, usado para estimar o melhor preço de venda para a casa de seus clientes.
